08cf298f202eace7dcad74c8fa92536aa8f6cb31
Dongdong Li (Andy) <lidongdong559@hotmail.com>
2015-10-22 22:13:38 -0400

    First Submission

---
+++ src/com/example/emaildemo/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.emaildemo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.ReceiverCallNotAllowedException;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+
+	EditText recipient;
+	EditText subject;
+	EditText message;
+	Button sendBtn;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        recipient=(EditText)findViewById(R.id.recipient);
+        subject=(EditText)findViewById(R.id.subject);
+        message=(EditText)findViewById(R.id.message);
+        sendBtn=(Button)findViewById(R.id.send);
+        
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View arg0) {
+				String[] recipientAddr={recipient.getText().toString()};
+				
+				Intent intent=new Intent(Intent.ACTION_SEND);
+				intent.setData(Uri.parse("mailto:"));
+				intent.setType("text/plain");
+				intent.putExtra(Intent.EXTRA_EMAIL, recipientAddr);
+				intent.putExtra(Intent.EXTRA_SUBJECT, subject.getText().toString());
+				intent.putExtra(Intent.EXTRA_TEXT, message.getText().toString());
+				
+				startActivity(Intent.createChooser(intent, "Send Email..."));
+				finish();
+				
+			}
+		});
+    }
+}
